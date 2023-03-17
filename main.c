2cf4a8195fca025a4cf48d7da8d9d0caa69f3664
João Ribeiro <joaaribeiro04.scbbracarense@gmail.com>
2023-03-17 17:17:39 +0000

    edit code

--- main.c
+++ main.c
@@ -209,30 +209,3 @@ int main() {
     return 0;
 }
 
-
-
-
-
-//   if (listagestor == NULL)
-//        return 1;
-//
-//    for (int i = 0; i < 2; i++){
-//       ReadManager(listagestor);
-//    }
-//    ShowManager(listagestor);
-//
-//    SetManager(listagestor, 20, "joao", "lalalala");
-//
-//    ShowManager(listagestor);
-//
-//    RmManager(listagestor, 21);
-//
-//    puts(""
-//    );
-//    puts("");
-//
-//    ShowManager(listagestor);
-//
-//    WriteManagerToTextFile(listagestor,"Gestor.txt");
-
-
