69c48d925ce632ea92df0b1bf3ac972fb942c0e1
João Ribeiro <joaaribeiro04.scbbracarense@gmail.com>
2023-05-26 19:04:48 +0100

    add lon lat in meio

---
+++ cmake-build-debug/CMakeFiles/EDA.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jp/Documents/EDA/cliente.c" "CMakeFiles/EDA.dir/cliente.c.o" "gcc" "CMakeFiles/EDA.dir/cliente.c.o.d"
+  "/home/jp/Documents/EDA/dlist.c" "CMakeFiles/EDA.dir/dlist.c.o" "gcc" "CMakeFiles/EDA.dir/dlist.c.o.d"
+  "/home/jp/Documents/EDA/gestor.c" "CMakeFiles/EDA.dir/gestor.c.o" "gcc" "CMakeFiles/EDA.dir/gestor.c.o.d"
+  "/home/jp/Documents/EDA/grafo.c" "CMakeFiles/EDA.dir/grafo.c.o" "gcc" "CMakeFiles/EDA.dir/grafo.c.o.d"
+  "/home/jp/Documents/EDA/main.c" "CMakeFiles/EDA.dir/main.c.o" "gcc" "CMakeFiles/EDA.dir/main.c.o.d"
+  "/home/jp/Documents/EDA/meio.c" "CMakeFiles/EDA.dir/meio.c.o" "gcc" "CMakeFiles/EDA.dir/meio.c.o.d"
+  "/home/jp/Documents/EDA/utils.c" "CMakeFiles/EDA.dir/utils.c.o" "gcc" "CMakeFiles/EDA.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/EDA.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA.dir/cliente.c.o"
+  "CMakeFiles/EDA.dir/cliente.c.o.d"
+  "CMakeFiles/EDA.dir/dlist.c.o"
+  "CMakeFiles/EDA.dir/dlist.c.o.d"
+  "CMakeFiles/EDA.dir/gestor.c.o"
+  "CMakeFiles/EDA.dir/gestor.c.o.d"
+  "CMakeFiles/EDA.dir/grafo.c.o"
+  "CMakeFiles/EDA.dir/grafo.c.o.d"
+  "CMakeFiles/EDA.dir/main.c.o"
+  "CMakeFiles/EDA.dir/main.c.o.d"
+  "CMakeFiles/EDA.dir/meio.c.o"
+  "CMakeFiles/EDA.dir/meio.c.o.d"
+  "CMakeFiles/EDA.dir/utils.c.o"
+  "CMakeFiles/EDA.dir/utils.c.o.d"
+  "EDA"
+  "EDA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
